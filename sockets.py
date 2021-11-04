c447d33e848e66f07a938736639f3f4d896236d4
TheOnlyMiki <jingzeng@ualberta.ca>
2021-11-03 21:47:03 -0600

    Finish copy from Assignment 4

--- sockets.py
+++ sockets.py
@@ -148,23 +148,28 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    #[ myWorld.update(entity, key, value) for key, value in flask_post_json().items() ]
+    myWorld.set(entity, flask_post_json())
+    # How to return JSON by Flask
+    # Source: https://stackoverflow.com/questions/13081532/return-json-response-from-flask-view
+    return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.world())
 
 
 
